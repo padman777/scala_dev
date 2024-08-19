881faa6455746bbb6ccc346d7b31755caf890a8e
padma <padman777@gmail.com>
2024-08-19 17:52:38 +0530

    assignment 1

---
+++ src/main/scala/chapter1.scala
@@ -0,0 +1,22 @@
+object chapter1 {
+  def main(args: Array[String]): Unit = {
+
+    //postive and divisible by 2
+    val a = 14
+
+    val ac = a > 0 && a % 2 == 0
+
+
+
+    // range check with or value less than -10 or greater than 10
+    val b = -15
+
+    val bc = b < -10 || b > 10
+
+    print (bc)
+
+    //
+
+
+  }
+}
