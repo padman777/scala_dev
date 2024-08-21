5995ec0eb5f79ecb1ceed0419bf90dc7ab086761
padma <padman777@gmail.com>
2024-08-21 08:13:43 +0530

    assignment 1

---
+++ src/main/scala/assignment1.scala
@@ -0,0 +1,50 @@
+object assignment1 {
+  def main(args:Array[String]):Unit ={
+
+//    // print numbers 1 to5 using for loop
+//
+//    for (i<- 1 to 5)
+//      println (i)
+
+// even numbers using while loop
+// var i=2
+//    while(i<=10 ) {
+//      println (i)
+//      i = i+2
+//
+//
+//      }
+
+    // sum of all numbers from 1 to 50 using for loop
+//    var sum=0
+//    for (i<- 1 to 50){
+//      sum=sum+i
+//
+//    }
+//    print (sum)
+
+    // square of numbers 1 to 5 using for loop
+//    var square =0
+//    for (i<- 1 to 5){
+//      square =i*i
+//
+//      println (square)
+
+    //first 5 multiples of 3
+
+
+
+
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+}
